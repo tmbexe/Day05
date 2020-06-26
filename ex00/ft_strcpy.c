df6e02a3297dceadc51bab2563daaf9afe0e4f97
Thamsanqa Mbexe <tmbexe@c4r13s5.wethinkcode.co.za>
2020-06-26 11:19:06 +0200

    Done with ex00

--- ex00/ft_strcpy.c
+++ ex00/ft_strcpy.c
@@ -6,10 +6,30 @@
 /*   By: tmbexe <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/25 15:31:32 by tmbexe            #+#    #+#             */
-/*   Updated: 2020/06/25 15:33:11 by tmbexe           ###   ########.fr       */
+/*   Updated: 2020/06/26 11:18:30 by tmbexe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
+#include <string.h>
 
-char ft_strcpy(char *dest, char *src);
+char ft_strcpy(char *dest, char *src){
+	int i;
+
+	i = 0;
+	while(src [i] != '\0'){
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = 0;
+	return '\0';
+}
+
+int main(){
+	char p[40] = {};
+	char z[] = "Hello World";
+	ft_strcpy(p, z);
+	printf("%s\n", p);
+	return 0;
+}
